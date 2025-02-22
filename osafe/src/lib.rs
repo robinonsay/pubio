4eb5beec5a6a5c6b8005d0f92e873ece16364d0e
Robin Onsay <hello@robinonsay.com>
2025-02-22 14:50:21 -0600

    :construction: add bindgen

--- osafe/src/lib.rs
+++ osafe/src/lib.rs
@@ -1,5 +1,12 @@
 #![no_std]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
 extern crate alloc;
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 pub mod error;
 pub mod io;
+pub mod multiprocessing;
